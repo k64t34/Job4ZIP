b1b6ab4cbb60ace7da7c0b1c2c1d4a0aec237a08
k64t34 <k64t34@gmail.com>
2018-05-31 13:22:01 +0300

    Init

---
+++ Job4ZIP/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: skorik
+ * Date: 31.05.2018
+ * Time: 11:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Job4ZIP
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
