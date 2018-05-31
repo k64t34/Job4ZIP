3f622e2e68ce10651619b3c803907d3d9c373632
k64t34 <k64t34@gmail.com>
2018-05-31 18:14:53 +0300

    31052018_1814

---
+++ Job4ZIP/Properties/Job4ZIP.job.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: skorik
+ * Date: 31.05.2018
+ * Time: 17:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace Job4ZIP
+{
+	
+}
---
+++ Job4ZIP/ReadFromXML.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: skorik
+ * Date: 31.05.2018
+ * Time: 15:01
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Job4ZIP
+{
+	/// <summary>
+	/// Description of ReadFromXML.
+	/// </summary>
+	public class ReadFromXML
+	{
+		public ReadFromXML()
+		{
+		}
+		//****************************************
+		bool ReadStringFromConfig()
+		//*************************************************		
+		{
+			/*Dim myel =From el In XmlDoc.<JOB>.Elements()  Where el.Name=argName Select el		 
+			var=myel.value   
+			If Strings.Left(var,1)="""" Then
+				If Strings.Right(var,1)="""" Then
+					var=Strings.Mid(var,2,Strings.Len(var)-2)
+				End If
+			End If*/
+			return true ;//len(var)<>0
+		}
+	}
+}
