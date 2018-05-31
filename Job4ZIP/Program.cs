c98aba31e1407619e91906dbbeeab5c91d59cb80
k64t34 <k64t34@gmail.com>
2018-05-31 13:34:26 +0300

    Add pressanykey downcount

--- Job4ZIP/Program.cs
+++ Job4ZIP/Program.cs
@@ -7,19 +7,37 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-
+using System.Threading; 
 namespace Job4ZIP
 {
 	class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			DateTime StartTime;
+			DateTime FinishTime;	
+			int DowncountInterval=5;//sec			
+			StartTime= DateTime.Now;
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.Clear();
+			Console.Title="Job for 7-Zip";
+			Console.WriteLine(Console.Title);
+			Console.WriteLine("------------------------------------------------------- by Skorik 2018");
+			Console.WriteLine("Start time {0}",StartTime);
+			FinishTime= DateTime.Now;
+			Console.Write("\nPress any key to continue . . . ");
+			while (DowncountInterval!=0)
+			{
+				Console.Write("\b{0}",DowncountInterval);
+				if (Console.KeyAvailable)
+				{
+					break;
+				}
+				Thread.Sleep(1000);
+				DowncountInterval--;
+			}
+			//Console.ReadKey(true);
 		}
 	}
 }
