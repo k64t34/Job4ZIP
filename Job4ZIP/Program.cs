c94e4e405be716c6bc02d8dd4dbd1a4d2feea121
k64t34 <k64t34@gmail.com>
2018-06-01 07:48:42 +0300

    31052018

--- Job4ZIP/Program.cs
+++ Job4ZIP/Program.cs
@@ -7,26 +7,98 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.Threading; 
+using System.Threading;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
 namespace Job4ZIP
 {
-	class Program
+	partial class Program
 	{
+		static int DowncountInterval=10;//sec
+		static XDocument XmlDoc;		
 		public static void Main(string[] args)
 		{
 			DateTime StartTime;
 			DateTime FinishTime;	
-			int DowncountInterval=5;//sec			
+			DateTime SpendTime;	
+			
+			String xmlFile;			
 			StartTime= DateTime.Now;
-			Console.BackgroundColor = ConsoleColor.White;
-			Console.ForegroundColor = ConsoleColor.Black;
+			//Console.BackgroundColor = ConsoleColor.Black;
+			//Console.ForegroundColor = ConsoleColor.White;
+			Console.ResetColor();
 			Console.Clear();
-			Console.Title="Job for 7-Zip";
-			Console.WriteLine(Console.Title);
-			Console.WriteLine("------------------------------------------------------- by Skorik 2018");
-			Console.WriteLine("Start time {0}",StartTime);
+			Console.Title=" Job for 7-Zip";
+			Console.WriteLine("-------------------------------------------------------");
+			Console.WriteLine("{0}                           {1}",Console.Title,"by Skorik 2018");
+			Console.WriteLine("-------------------------------------------------------");
+			Console.WriteLine("Start time\t{0}",StartTime);
+			//
+			//Parsing arguments command line
+			//
+			xmlFile=AppDomain.CurrentDomain.BaseDirectory;
+			if (args.Length==0) 
+				xmlFile+="job4zip.xml";
+			else				 
+				xmlFile+=args[0];
+			
+			Console.WriteLine("Config file is {0}",xmlFile);
+			
+			if (!File.Exists(xmlFile))
+			{
+				ShowError_Exit(String.Format("ERR: Config File \"{0}\" not exist",xmlFile),1);	
+			}
+			// 
+            //Parsing config file
+            //
+            
+            XmlDoc=new XDocument();
+			try
+			{
+				XmlDoc = XDocument.Load(xmlFile);
+			}
+        	catch (Exception ex)
+			{							
+        		Console.ForegroundColor = ConsoleColor.DarkRed;
+        		Console.WriteLine("ERR: Config File \"{0}\" not parsing.",xmlFile);
+        		Console.ForegroundColor = ConsoleColor.DarkYellow;
+        		Console.WriteLine(ex.Message);
+        		ShowError_Exit("",2);	        	 
+			}
+			
+			
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine(XmlDoc);
+			Console.ResetColor();
+			
+			if (XmlDoc.Element("PLAN")==null)
+				ShowError_Exit("Root Tag PLAN not found in config file",30);
+			
+			if (XmlDoc.Element("PLAN").Element("JOBs")==null)
+				ShowError_Exit("Tag JOBs not found in config file",31);
+			
+			XElement JOBs = XmlDoc.Element("PLAN").Element("JOBs");
+			foreach ( XElement job in JOBs.Elements("JOB"))
+			{
+				doJob(job);//Console.WriteLine(job);
+			}
+				
+			//Console.ResetColor();Console.ForegroundColor = ConsoleColor.White;
+			
+			Console.WriteLine("Start time\t{0}",StartTime);			
 			FinishTime= DateTime.Now;
-			Console.Write("\nPress any key to continue . . . ");
+			Console.WriteLine("Finish time\t{0}",FinishTime);	
+			//SpendTime=FinishTime-StartTime;
+			Console.WriteLine("Spend time\t{0} sec",(FinishTime - StartTime).Seconds);
+			FinishDownCount();
+			
+		}
+		//*********************************
+		static void FinishDownCount() {
+		//*********************************
+		 Console.Write("\nPress any key to continue . . . ");
 			while (DowncountInterval!=0)
 			{
 				Console.Write("\b{0}",DowncountInterval);
@@ -36,8 +108,19 @@ namespace Job4ZIP
 				}
 				Thread.Sleep(1000);
 				DowncountInterval--;
-			}
-			//Console.ReadKey(true);
+			} 
+		}  
+		//*********************************
+		static void ShowError_Exit(String Message, int ExitCode=1){
+		//*********************************	
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+        	Console.WriteLine(Message);        	
+        	Console.ResetColor();
+	       	FinishDownCount();
+			Environment.ExitCode = ExitCode;
+			Environment.Exit(ExitCode); 
 		}
+		
+ 
 	}
 }
--- Job4ZIP/Properties/Job4ZIP.job.cs
+++ Job4ZIP/Properties/Job4ZIP.job.cs
@@ -6,8 +6,16 @@
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
-
+using System.Xml.Linq;
+using System;
 namespace Job4ZIP
 {
+	partial  class Program
+	{
+	static void doJob(XElement job)
+	{
 	
+		Console.WriteLine(job.Attribute("id"));
+	}
+	}
 }
